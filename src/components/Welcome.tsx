8f1783d108a7224db404a00a83c6ebd730577f02
mantraStudios <manichost@proton.me>
2024-01-05 14:36:44 -0800

    changesss

--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,15 +1,10 @@
 export const Welcome: React.FC = () => (
   <div className="bg-white border-gray-100 border-2 rounded-lg px-8 py-5 mr-20 w-full">
-    <h1 className="text-2xl font-bold mb-2">🤖 Hello, I am Daneel</h1>
+        <h1 className="text-2xl font-bold mb-2">👽 Hello, I am MasterBot5000</h1>
     <p>
-      You can use me to create your own chat bot app. In this demo I am an
-      expert in movie reviews, so feel free to ask me about movies! Then, create
-      your own bot in under five minutes.
+      As a General domain expert, your role is to provide a deep level of understanding and knowledge in various areas. Start asking questions below!
     </p>
     <p>
-      <a className="underline" href="https://github.com/ascorbic/daneel#readme">
-        Build your own
-      </a>
     </p>
   </div>
 );
